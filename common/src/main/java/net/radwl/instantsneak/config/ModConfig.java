2e1c10bb8428038aaf90d6b45beb3047c076cb29
radwl <raduiliusco@gmail.com>
2025-12-01 00:39:30 +0200

    Updated to Minecraft 1.21.10.

--- common/src/main/java/net/radwl/instantsneak/config/ModConfig.java
+++ common/src/main/java/net/radwl/instantsneak/config/ModConfig.java
@@ -5,7 +5,7 @@ import eu.midnightdust.lib.config.MidnightConfig;
 public class ModConfig extends MidnightConfig {
 
     @Entry() public static boolean INSTANT = true;
-    @Entry(isSlider = true, min = 0.0, max = 2.0) public static double ANIMATION_SPEED = 1.0;
+    @Entry(isSlider = true, min = 0.5, max = 2.0) public static double ANIMATION_SPEED = 1.0;
     @Entry(isSlider = true, min = 0.0, max = 0.75) public static double SNEAK_HEIGHT = 0.7;
 
 }
